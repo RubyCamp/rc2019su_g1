e397d5b39447a42378736e4f5adbe219fcba212f
日高 広夢 <chiro3330@i.softbank.jp>
2019-08-20 17:18:40 +0900

    修正後

--- scenes/game/GameManager.rb
+++ scenes/game/GameManager.rb
@@ -8,15 +8,7 @@ module Game
 
     def play
       Window.draw_font(0,0,"ｓｓｓｓ", @@font)
-
-<<<<<<< HEAD
       if Input.keyPush?(K_A)
-=======
-      @timer -= 1
-      if @timer <= 0
-        @timer = 3*60
-
->>>>>>> 44b59aa9afa36099cecad9a81ed65fb2befd412b
         Scene.move_to(:result)
       end
     end
--- scenes/result/ResultManager.rb
+++ scenes/result/ResultManager.rb
@@ -9,13 +9,7 @@ module Result
     def play
       Window.draw_font(0,0,"ああああ", @@font)
 
-<<<<<<< HEAD
       if Input.keyPush?(K_S)
-=======
-      @timer -= 1
-      if @timer <= 0
-        @timer = 3*60
->>>>>>> 44b59aa9afa36099cecad9a81ed65fb2befd412b
         Scene.move_to(:title)
       end
     end
